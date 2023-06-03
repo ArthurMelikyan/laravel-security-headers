1b9ae38d8bdd3c69959a0da092b6930e465f3ddd
Arthur Melikyan <arthmelikyan@gmail.com>
2023-06-03 23:01:56 +0400

    Update HeadersSettings.php

--- HeadersSettings.php
+++ HeadersSettings.php
@@ -18,9 +18,9 @@ class HeadersSettings
         $response->headers->set('X-XSS-Protection', '1; mode=block');
         $response->headers->set('X-Frame-Options', 'SAMEORIGIN');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
-        $response->headers->set('Cache-Control','nocache, no-store, max-age=0, must-revalidate');
-        $response->headers->set('Pragma','no-cache');
-        $response->headers->set('Expires','Fri, 01 Jan 1990 00:00:00 GMT');
+        $response->headers->set('Cache-Control', 'nocache, no-store, max-age=0, must-revalidate');
+        $response->headers->set('Pragma', 'no-cache');
+        $response->headers->set('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
 
         return $response;
     }
